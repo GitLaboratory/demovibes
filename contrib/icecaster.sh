17e3da82727cfc54a298d200dfbeb079d98ad9f4
FishGuy8765 <FishGuy8765@5733aaf2-ac46-11de-892d-7b2b76e7707e>
2009-12-03 18:09:45 +0000

    This will force ices stream to automatically restart in the event it craps out for any reason.

--- contrib/icecaster.sh
+++ contrib/icecaster.sh
@@ -12,10 +12,17 @@ url="http://__URL__/"
 genre="scenemusic"
 name="DemovibesDev"
 crossfade="0" # Crossfade seconds
-extra_opts="" # -R for reencode, -s for private stream
-
+extra_opts="-R 3 -N 2 -H 44100" # -R for reencode, -N Channel count, -H Bitrate Output
 
 pymodule="pyAdder"
 password=`cat icepw.txt` #Read password from file
 
-$ices -h $host -b $bitrate -d "$description" -p $port -P "$password" -S python -M $pymodule -u $url -D $tmpdir -g "$genre" -n "$name" $extra_opts
+while /bin/true
+do
+        echo "Restarting ICES Stream"
+        $ices -h $host -b $bitrate -d "$description" -p $port -P "$password" -S python -M $pymodule -u $url -D $tmpdir -g "$genre" -n "$name" $extra_opts
+
+        # Issue a brief pause on failure; This gives time for ices0 memory to be released back
+        # To the system. The stream will then aut-restart.
+        sleep 10
+done
